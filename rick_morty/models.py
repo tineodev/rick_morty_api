a212fdd7d10a82346e8ae7c6832d04c2f64e3c4f
tineodev <tineo.dev@gmail.com>
2022-12-03 09:46:31 -0500

    Add model DB

--- rick_morty/models.py
+++ rick_morty/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    species = models.CharField(max_length=20)
+    type_ = models.CharField(max_length=50)
+    gender = models.CharField(max_length=20)
+    origin_name = models.CharField(max_length=100)
+    location_name = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
+    episode = models.CharField(max_length=100)
+    # ! completar created = models.
+
+
+
