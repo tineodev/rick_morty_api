fc48feca919b66616d42daf6d131ac247c7fe5e5
tineodev <tineo.dev@gmail.com>
2022-12-03 11:29:35 -0500

    Add function request_api

--- rick_morty/request_api.py
+++ rick_morty/request_api.py
@@ -1,7 +1,22 @@
 import urllib.request, json
 
-def request_api(pm_valor):
+def request_api(pm_valor, pm_model):
     for i in range (1,pm_valor):
-        url_api = f'https://rickandmortyapi.com/api/character?page={pm_valor}'
+        url_api = f'https://rickandmortyapi.com/api/character?page={i}'
         response = urllib.request.urlopen(url_api)
-        libros = json.loads(response.read())
+        libro = json.loads(response.read())
+
+        for i in libro['results']:
+            pm_name =i['name']
+            pm_status = i['status']
+            pm_species =i['species']
+            pm_type =i['type']
+            pm_gender =i['gender']
+            pm_origin =i['origin']['name']
+            pm_location =i['location']['name']
+            pm_image =i['image']
+            #! pm_episode =i['episode']
+            pm_created =i['created']
+
+
+            pm_model.objects.create(name=pm_name, status =pm_status, species=pm_species,type_r=pm_type, gender=pm_gender, origin_name=pm_origin, location_name=pm_location, image=pm_image, created=pm_created)
--- rick_morty/views.py
+++ rick_morty/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 
 from django.views.generic import View
 
+from .models import Pokemon
+from .request_api import request_api
+import urllib.request, json
+
+
 # Create your views here.
 
 class Index(View):
@@ -12,8 +17,10 @@ class Index(View):
 
 class GenerateDB(View):
     def get(self,request):
+        request_api(2,Pokemon)
         return redirect('index')
 
 class DeleteDB(View):
     def get(self,request):
+        Pokemon.objects.all().delete()
         return redirect('index')
