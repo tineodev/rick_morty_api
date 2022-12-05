5e03b39e8a113db40f392b2a7ef4b3083197978d
tineodev <tineo.dev@gmail.com>
2022-12-05 10:26:02 -0500

    Add routes and redirections

--- rick_morty/urls.py
+++ rick_morty/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth.views import LoginView, logout_then_login
+
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
-    path("generate/", views.GenerateDB.as_view(), name="generate"),
-    path("delete/", views.DeleteDB.as_view(), name="delete"),
+
     path("list/", views.ListDB.as_view(), name="list"),
     path("detail/<int:id>", views.DetailDB.as_view(), name="detail"),
+
+    path("main/", views.MainPage.as_view(), name="main"),
+    path("generate/", views.GenerateDB.as_view(), name="generate"),
+    path("delete/", views.DeleteDB.as_view(), name="delete"),
+
+    path("accounts/login/", LoginView.as_view(), name="login"),
+    path("register/", CreateUser.as_view(), name="register"),
+    path("logout/", logout_then_login, name="logout"),
 ]
--- zproject/settings.py
+++ zproject/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# URL redirects
+LOGIN_URL = 'index'
+LOGIN_REDIRECT_URL = 'main'
