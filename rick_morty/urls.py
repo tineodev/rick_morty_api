53c3f0116446e9a9f243ebc82d8c24457a916e88
tineodev <tineo.dev@gmail.com>
2022-12-05 10:08:56 -0500

    Minimal change

--- rick_morty/urls.py
+++ rick_morty/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index/", views.Index.as_view(), name="index"),
+    path("", views.Index.as_view(), name="index"),
     path("generate/", views.GenerateDB.as_view(), name="generate"),
     path("delete/", views.DeleteDB.as_view(), name="delete"),
     path("list/", views.ListDB.as_view(), name="list"),
